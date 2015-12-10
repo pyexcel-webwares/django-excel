d862473e5c645310db77f9ae0314cad3ec447f7a
chfw <wangc_2011@hotmail.com>
2015-12-10 22:24:18 +0000

    pump up version

--- setup.py
+++ setup.py
@@ -13,15 +13,24 @@ dependencies = [
     'pyexcel-webio>=0.0.2',
     'Django>=1.7.1'
 ]
+extras = {
+    'xls': ['pyexcel-xls>=0.0.7'],
+    'xlsx': ['pyexcel-xlsx>=0.0.7'],
+    'ods3': [
+        'pyexcel-ods3>=0.0.8',
+        'https://github.com/T0ha/ezodf/archive/2c69103e6c0715adb0e36562cb2e6325fd776112.zip',
+    ],
+}
 
 setup(
     name='django-excel',
     author="C. W.",
-    version='0.0.2',
+    version='0.0.3',
     author_email="wangc_2011@hotmail.com",
     url="https://github.com/chfw/django-excel",
     description='A django middleware that provides one application programming interface to read and write data in different excel file formats',
     install_requires=dependencies,
+    extras_require=extras,
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'mysite', 'polls', 'migrations']),
     include_package_data=True,
     long_description=README_txt,
@@ -37,6 +46,7 @@ setup(
         'Programming Language :: Python',
         'License :: OSI Approved :: BSD License',
         'Intended Audience :: Developers',
+        'Framework :: Django :: 1.7',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',

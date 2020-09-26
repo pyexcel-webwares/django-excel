8200b22c5d61aeb288610d2626a4acaa74def40f
chfw <wangc_2011@hotmail.com>
2020-09-26 22:43:49 +0100

    :books: update all meta data

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 pip freeze
-nosetests --with-coverage --cover-package django_excel --cover-package tests tests --with-doctest --doctest-extension=.rst README.rst docs/source django_excel
+coverage run --source='django_excel' manage.py test

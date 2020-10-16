348f8c903b23e38903da56a8cc12b176243141f6
chfw <wangc_2011@hotmail.com>
2020-10-16 22:40:58 +0100

    :handshake: update project meta data

--- setup.py
+++ setup.py
@@ -93,13 +93,14 @@ EXTRAS_REQUIRE = {
 }
 # You do not need to read beyond this line
 PUBLISH_COMMAND = "{0} setup.py sdist bdist_wheel upload -r pypi".format(sys.executable)
+HERE = os.path.abspath(os.path.dirname(__file__))
+
 GS_COMMAND = ("gs django-excel v0.0.10 " +
               "Find 0.0.10 in changelog for more details")
 NO_GS_MESSAGE = ("Automatic github release is disabled. " +
                  "Please install gease to enable it.")
 UPLOAD_FAILED_MSG = (
     'Upload failed. please run "%s" yourself.' % PUBLISH_COMMAND)
-HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 class PublishCommand(Command):
@@ -144,7 +145,6 @@ class PublishCommand(Command):
 SETUP_COMMANDS.update({
     "publish": PublishCommand
 })
-
 
 def has_gease():
     """

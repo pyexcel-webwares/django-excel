60ebd20b100bf5938e100d102f48af1bcb0b581b
jaska <wangc_2011@hotmail.com>
2020-10-24 14:18:43 +0100

    Update lint.sh

--- lint.sh
+++ lint.sh
@@ -1,4 +1,4 @@
 pip install flake8
 
-flake8 --exclude=.moban.d,docs,setup.py --ignore=F401,E402  --builtins=unicode,xrange,long .  && python setup.py checkdocs
+flake8 --exclude=.moban.d,docs,setup.py --ignore=F401,E402,W504,E226  --builtins=unicode,xrange,long .  && python setup.py checkdocs
 

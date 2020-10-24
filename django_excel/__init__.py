a3f2149b4c0c6010ed7c0bcd66c0f2b86c2b0cdd
Minh-Long Do <minhlong.langos@gmail.com>
2020-10-24 20:42:53 +0700

    Make library django 3 compatible (#64)
    * Exclude venv folder from flake8
    
    * Use MIDDLEWARE instead of MIDDLEWARE_CLASSES if django version is greater than 1
    
    * Ran 'make format'
    
    * Update changelog
    
    * Update django_excel.yml
    
    * Update lint.sh
    
    * Update lint.sh
    
    * Update .travis.yml
    
    * Update django_excel.yml
    
    Co-authored-by: jaska <wangc_2011@hotmail.com>

--- django_excel/__init__.py
+++ django_excel/__init__.py
@@ -8,13 +8,18 @@
     :copyright: (c) 2015 by Onni Software Ltd.
     :license: New BSD License
 """
-from django.core.files.uploadhandler import (
-    MemoryFileUploadHandler, TemporaryFileUploadHandler)
-from django.core.files.uploadedfile import (
-    InMemoryUploadedFile, TemporaryUploadedFile)
-from django.http import HttpResponse
 import pyexcel as pe
 import pyexcel_webio as webio
+from django.core.files.uploadedfile import (
+    InMemoryUploadedFile,
+    TemporaryUploadedFile,
+)
+from django.core.files.uploadhandler import (
+    MemoryFileUploadHandler,
+    TemporaryFileUploadHandler,
+)
+from django.http import HttpResponse
+
 from ._compact import DJANGO_ONE_SIX, PY2_VERSION, urllib_quote
 
 
@@ -22,70 +27,85 @@ class ExcelMixin(webio.ExcelInput):
     """
     Provide additional pyexcel-webio methods to Django's UploadedFiles
     """
+
     def get_params(self, **keywords):
         extension = self.name.split(".")[-1]
-        keywords['file_type'] = extension
+        keywords["file_type"] = extension
         self.file.seek(0)
         content = self.file.read()
         if content:
-            keywords['file_content'] = content
+            keywords["file_content"] = content
         else:
             raise IOError("No content was uploaded.")
         return keywords
 
-    def save_to_database(self, model=None, initializer=None, mapdict=None,
-                         **keywords):
+    def save_to_database(
+        self, model=None, initializer=None, mapdict=None, **keywords
+    ):
         """
         Save data from a sheet to a nominated django model
         """
         params = self.get_params(**keywords)
-        if 'name_columns_by_row' not in params:
-            params['name_columns_by_row'] = 0
-        if 'name_rows_by_column' not in params:
-            params['name_rows_by_column'] = -1
-        params['dest_model'] = model
-        params['dest_initializer'] = initializer
-        params['dest_mapdict'] = mapdict
+        if "name_columns_by_row" not in params:
+            params["name_columns_by_row"] = 0
+        if "name_rows_by_column" not in params:
+            params["name_rows_by_column"] = -1
+        params["dest_model"] = model
+        params["dest_initializer"] = initializer
+        params["dest_mapdict"] = mapdict
         pe.save_as(**params)
 
-    def save_book_to_database(self, models=None, initializers=None,
-                              mapdicts=None, batch_size=None, bulk_save=None,
-                              **keywords):
+    def save_book_to_database(
+        self,
+        models=None,
+        initializers=None,
+        mapdicts=None,
+        batch_size=None,
+        bulk_save=None,
+        **keywords
+    ):
         """
         Save data from a book to a nominated django models
         """
         params = self.get_params(**keywords)
-        params['dest_models'] = models
-        params['dest_initializers'] = initializers
-        params['dest_mapdicts'] = mapdicts
-        params['dest_batch_size'] = batch_size
-        params['dest_bulk_save'] = bulk_save
+        params["dest_models"] = models
+        params["dest_initializers"] = initializers
+        params["dest_mapdicts"] = mapdicts
+        params["dest_batch_size"] = batch_size
+        params["dest_bulk_save"] = bulk_save
         pe.save_book_as(**params)
 
-    def isave_to_database(self, model=None, initializer=None, mapdict=None,
-                          **keywords):
+    def isave_to_database(
+        self, model=None, initializer=None, mapdict=None, **keywords
+    ):
         """
         Save data from a sheet to a nominated django model
         """
         params = self.get_params(**keywords)
-        params['dest_model'] = model
-        params['dest_initializer'] = initializer
-        params['dest_mapdict'] = mapdict
+        params["dest_model"] = model
+        params["dest_initializer"] = initializer
+        params["dest_mapdict"] = mapdict
         pe.isave_as(**params)
         self.free_resources()
 
-    def isave_book_to_database(self, models=None, initializers=None,
-                               mapdicts=None, batch_size=None, bulk_save=None,
-                               **keywords):
+    def isave_book_to_database(
+        self,
+        models=None,
+        initializers=None,
+        mapdicts=None,
+        batch_size=None,
+        bulk_save=None,
+        **keywords
+    ):
         """
         Save data from a book to a nominated django models
         """
         params = self.get_params(**keywords)
-        params['dest_models'] = models
-        params['dest_initializers'] = initializers
-        params['dest_mapdicts'] = mapdicts
-        params['dest_batch_size'] = batch_size
-        params['dest_bulk_save'] = bulk_save
+        params["dest_models"] = models
+        params["dest_initializers"] = initializers
+        params["dest_mapdicts"] = mapdicts
+        params["dest_batch_size"] = batch_size
+        params["dest_bulk_save"] = bulk_save
         pe.isave_book_as(**params)
 
 
@@ -93,6 +113,7 @@ class ExcelInMemoryUploadedFile(ExcelMixin, InMemoryUploadedFile):
     """
     Mix-in pyexcel-webio methods in InMemoryUploadedFile
     """
+
     pass
 
 
@@ -100,6 +121,7 @@ class TemporaryUploadedExcelFile(ExcelMixin, TemporaryUploadedFile):
     """
     Mix-in pyexcel-webio methods in TemporaryUploadedFile
     """
+
     pass
 
 
@@ -107,6 +129,7 @@ class ExcelMemoryFileUploadHandler(MemoryFileUploadHandler):
     """
     Override MemoryFileUploadHandler to bring in ExcelInMemoryUploadedFile
     """
+
     def file_complete(self, file_size):
         if not self.activated:
             return
@@ -117,7 +140,7 @@ class ExcelMemoryFileUploadHandler(MemoryFileUploadHandler):
             name=self.file_name,
             content_type=self.content_type,
             size=file_size,
-            charset=self.charset
+            charset=self.charset,
         )
         if not DJANGO_ONE_SIX:
             keywords["content_type_extra"] = self.content_type_extra
@@ -128,19 +151,15 @@ class TemporaryExcelFileUploadHandler(TemporaryFileUploadHandler):
     """
     Override TemporaryFileUploadHandler to bring in TemporaryUploadedExcelFile
     """
+
     def new_file(self, file_name, *args, **kwargs):
         """
         Create the file object to append to as data is coming in.
         """
         super(TemporaryFileUploadHandler, self).new_file(
-            file_name,
-            *args,
-            **kwargs)
-        custom_args = [
-            self.file_name,
-            self.content_type,
-            0,
-            self.charset]
+            file_name, *args, **kwargs
+        )
+        custom_args = [self.file_name, self.content_type, 0, self.charset]
         if not DJANGO_ONE_SIX:
             custom_args.append(self.content_type_extra)
         self.file = TemporaryUploadedExcelFile(*custom_args)
@@ -153,11 +172,13 @@ def _make_response(content, content_type, status, file_name=None):
     response = HttpResponse(content, content_type=content_type, status=status)
     if file_name:
         if PY2_VERSION and isinstance(file_name, unicode):
-            file_name = file_name.encode('utf-8')
+            file_name = file_name.encode("utf-8")
         url_encoded_file_name = urllib_quote(file_name)
-        response["Content-Disposition"] = (
-            "attachment; filename=%s;filename*=utf-8''%s"
-            % (url_encoded_file_name, url_encoded_file_name)
+        response[
+            "Content-Disposition"
+        ] = "attachment; filename=%s;filename*=utf-8''%s" % (
+            url_encoded_file_name,
+            url_encoded_file_name,
         )
     return response
 
@@ -165,18 +186,19 @@ def _make_response(content, content_type, status, file_name=None):
 webio.init_webio(_make_response)
 
 
-from pyexcel_webio import (  # noqa
+from pyexcel_webio import (
     make_response,
-    make_response_from_array,
-    make_response_from_dict,
-    make_response_from_records,
+    make_response_from_array,  # noqa
     make_response_from_book_dict,
-    make_response_from_query_sets
+    make_response_from_dict,
+    make_response_from_query_sets,
+    make_response_from_records,
 )
 
 
-def make_response_from_a_table(model, file_type,
-                               status=200, file_name=None, **keywords):
+def make_response_from_a_table(
+    model, file_type, status=200, file_name=None, **keywords
+):
     """
     Produce a single sheet Excel book of *file_type*
 
@@ -185,12 +207,14 @@ def make_response_from_a_table(model, file_type,
     :param status: same as :meth:`~django_excel.make_response`
     """
     sheet = pe.get_sheet(model=model, **keywords)
-    return make_response(sheet, file_type, status,
-                         file_name=file_name, **keywords)
+    return make_response(
+        sheet, file_type, status, file_name=file_name, **keywords
+    )
 
 
-def make_response_from_tables(models, file_type,
-                              status=200, file_name=None, **keywords):
+def make_response_from_tables(
+    models, file_type, status=200, file_name=None, **keywords
+):
     """
     Produce a multiple sheet Excel book of *file_type*. It becomes the same
     as :meth:`~django_excel.make_response_from_a_table` if you pass *tables*
@@ -201,5 +225,6 @@ def make_response_from_tables(models, file_type,
     :param status: same as :meth:`~django_excel.make_response`
     """
     book = pe.get_book(models=models, **keywords)
-    return make_response(book, file_type, status,
-                         file_name=file_name, **keywords)
+    return make_response(
+        book, file_type, status, file_name=file_name, **keywords
+    )
--- django_excel/_compact.py
+++ django_excel/_compact.py
@@ -1,20 +1,22 @@
-import pkg_resources
 from distutils.version import LooseVersion
+
+import pkg_resources
 
 try:
     # if in py2
     from urllib import quote as urllib_quote
+
     PY2_VERSION = True
 except ImportError:
     # else (aka in py3)
     from urllib.parse import quote as urllib_quote  # noqa: F401
+
     PY2_VERSION = False
 
 
-django_version = pkg_resources.get_distribution('django').version
+django_version = pkg_resources.get_distribution("django").version
 
 
-DJANGO_ONE_SIX = (
-    LooseVersion(django_version) < LooseVersion("1.7.0") and
-    LooseVersion(django_version) > LooseVersion("1.5.12")
-)
+DJANGO_ONE_SIX = LooseVersion(django_version) < LooseVersion(
+    "1.7.0"
+) and LooseVersion(django_version) > LooseVersion("1.5.12")
